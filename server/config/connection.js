0401bcfdd7c86dc84383653f139f7cb45ab233de
SNOODBOOK\MiniSnood <duncaniaria@gmail.com>
2017-08-21 23:00:49 -0400

    removed connection.js content (commented out for now)

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,43 +1,43 @@
-const mysql = require( 'mysql2' );
-let connection;
+// const mysql = require( 'mysql2' );
+// let connection;
 
-//use JAWSDB if it's detected/otherwise use local
-if( process.env.JAWSDB_URL )
-{
-    connection = mysql.createConnection( process.env.JAWSDB_URL );
-}
-else
-{
-    connection = mysql.createConnection
-    (
-        {
-            host: process.env.DB_HOST,
-            port: process.env.DB_PORT,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME
-        }
-    );
-}
+// //use JAWSDB if it's detected/otherwise use local
+// if( process.env.JAWSDB_URL )
+// {
+//     connection = mysql.createConnection( process.env.JAWSDB_URL );
+// }
+// else
+// {
+//     connection = mysql.createConnection
+//     (
+//         {
+//             host: process.env.DB_HOST,
+//             port: process.env.DB_PORT,
+//             user: process.env.DB_USER,
+//             password: process.env.DB_PASSWORD,
+//             database: process.env.DB_NAME
+//         }
+//     );
+// }
 
-//=========================
-//  CONNECTION LOGIC
-//=========================
-connection.connect( onConnectionComplete );
+// //=========================
+// //  CONNECTION LOGIC
+// //=========================
+// connection.connect( onConnectionComplete );
 
-function onConnectionComplete( tError )
-{
-    if( tError )
-    {
-        console.log( `there was an error when connecting: ${ tError }` );
-    }
-    else
-    {
-        console.log( `MySQL connection complete at port ${ process.env.DB_PORT }` );
-    }
-}
+// function onConnectionComplete( tError )
+// {
+//     if( tError )
+//     {
+//         console.log( `there was an error when connecting: ${ tError }` );
+//     }
+//     else
+//     {
+//         console.log( `MySQL connection complete at port ${ process.env.DB_PORT }` );
+//     }
+// }
 
-//=========================
-//  EXPORT
-//=========================
-module.exports = connection;
+// //=========================
+// //  EXPORT
+// //=========================
+// module.exports = connection;
